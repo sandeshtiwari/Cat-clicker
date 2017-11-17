47a842c87c764983eeaefa108954993f6fddb53a
Sandesh Tiwari <sandeshtiwari@live.com>
2017-11-17 11:26:33 -0600

    setup the model for the app

---
+++ myJS.js
@@ -0,0 +1,15 @@
+var model = {
+    currentCat : null,
+    cat : [
+        {
+            name : 'Hehe',
+            countClick : 0,
+            source : "cat1.jpg" 
+        },
+        {
+            name: 'haha',
+            countClick: 0,
+            source: 'cat2.jpg'
+        }
+    ]
+};
